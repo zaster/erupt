4a5e3505aa921590700b43ff8ddefd55858567bd
Hansch <zasterliu@gmail.com>
2021-09-28 16:32:27 +0800

    row operation importx bug fix update

--- erupt-core/src/main/java/xyz/erupt/core/naming/EruptRowOperationConfig.java
+++ erupt-core/src/main/java/xyz/erupt/core/naming/EruptRowOperationConfig.java
@@ -1,20 +1,21 @@
 package xyz.erupt.core.naming;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Component;
+
 import xyz.erupt.annotation.sub_erupt.RowOperation;
 import xyz.erupt.core.annotation.EruptRecordOperate;
 import xyz.erupt.core.controller.EruptDataController;
 import xyz.erupt.core.service.EruptCoreService;
 import xyz.erupt.core.view.EruptModel;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-
 /**
- * @author YuePeng
- * date 2021/5/7 10:28
+ * @author YuePeng date 2021/5/7 10:28
  */
 @Component
 public class EruptRowOperationConfig implements EruptRecordOperate.DynamicConfig {
@@ -30,9 +31,10 @@ public class EruptRowOperationConfig implements EruptRecordOperate.DynamicConfig
     }
 
     private RowOperation findRowOperation(EruptModel eruptModel) {
-        String code = request.getServletPath().split(EruptDataController.OPERATOR_PATH_STR + "/")[1];
-        return Arrays.stream(eruptModel.getErupt().rowOperation())
-                .filter(operation -> operation.code().equals(code)).findFirst()
+        String code = request.getServletPath().replace("/importx", "")
+                .split(EruptDataController.OPERATOR_PATH_STR + "/")[1];
+        return Arrays.stream(eruptModel.getErupt().rowOperation()).filter(operation -> operation.code().equals(code))
+                .findFirst()
                 .orElseThrow(() -> new RuntimeException(eruptModel.getEruptName() + " RowOperation not found " + code));
     }
 }
